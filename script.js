9be73c2d89105d402cece6de5cfc2e5c4cfadbbb
Sukhinina Lila <soukhinina@yahoo.com>
2025-01-24 12:32:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// function afficherPointeSapin(hauteur) {
+
+//   }
+  
+//   afficherPointeSapin(4)
+
+  function afficherEtoiles(n) {
+    let etoile = '';
+    for(let i = 0; i < n; i++){
+        etoile += '*';
+    }
+    console.log(etoile);
+  }
+  afficherEtoiles(2);
+  afficherEtoiles(5);
+
+  function afficherRectangle(hauteur,largeur){
+    
+    }
+  afficherRectangle(5,5);
